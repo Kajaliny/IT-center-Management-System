aaf5f7e35a6595d4f15305ef000ca6440a2aa57c
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2024-06-21 23:06:43 +0530

    Update login.js

---
+++ login.js
@@ -0,0 +1,25 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    try {
+        console.log("data",email,password);
+        const response = await fetch('http://192.168.81.91:5000/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const data = await response.json();
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            window.location.href = 'dashboard.html';
+        } else {
+            alert(data.error);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
